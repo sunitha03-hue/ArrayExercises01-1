97887c69124d83582c8590d59c40c0ea2a7357c1
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-02-25 20:25:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Create an array with the following entry: 273.15.
+
+
+//Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
+
+
+
+//Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
