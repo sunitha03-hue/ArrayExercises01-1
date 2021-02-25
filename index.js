1bc28426a3611154ca5693b45db3bde0f8efd51a
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-02-25 20:48:22 +0000

    Java Exercise 8.5 Part 1 completed

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //Create an array with the following entry: 273.15.
-
+let emptyArray = [273.15];
 
 //Use the push method to add the number 42 and the string "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
-
+emptyArray.push(42);
+emptyArray.push('hello');
 
 //Use a single push to add the items false, -4.6, and “87” to the array.  Print the array to confirm the changes.
+emptyArray.push(false, -4.6, "87");
+console.log(emptyArray);
+  
